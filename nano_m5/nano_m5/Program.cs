4949c838eb09ddd1c35ade73c2354d0328728af0
AtaruMatsudaira <matsu222212@gmail.com>
2022-11-07 21:29:52 +0900

    feat:ジャイロセンサーの値を取得

--- nano_m5/nano_m5/Program.cs
+++ nano_m5/nano_m5/Program.cs
@@ -1,4 +1,5 @@
 using nanoFramework.M5Stack;
+using System.Diagnostics;
 using Console = nanoFramework.M5Stack.Console;
 
 namespace HelloWorld
@@ -8,8 +9,12 @@ namespace HelloWorld
         public static void Main()
         {
             Setup();
-
             Console.WriteLine("HelloWorld");
+            while (true)
+            {
+                Loop();
+                System.Threading.Thread.Sleep(50);
+            }
         }
 
         private static void Setup()
@@ -17,6 +22,17 @@ namespace HelloWorld
             M5Core2.InitializeScreen();
             Console.Clear();
             Console.ForegroundColor = nanoFramework.Presentation.Media.Color.White;
+        }
+
+        private static void Loop()
+        {
+            var ag = M5Core2.AccelerometerGyroscope;
+            ag.Calibrate(100);
+            var acc = ag.GetAccelerometer();
+            var gyr = ag.GetGyroscope();
+            Console.Clear();
+            Console.WriteLine($"Accelerometer data x:{acc.X} y:{acc.Y} z:{acc.Z}");
+            Console.WriteLine($"Gyroscope data x:{gyr.X} y:{gyr.Y} z:{gyr.Z}\n");
         }
     }
 }
